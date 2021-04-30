7d7c830d377c44ba3c907da41762dc25b83ce698
your username <59005110+pappu8871@users.noreply.github.com>
2021-04-30 23:37:01 +0530

    Added:flippin a coin to display Head or Tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-
-
-echo "Welcome to flipCoinCombination"
+Head=0
+Tils=1
+REsult=$((RANDOM%2))
+if [[ ${Result} -eq 0 ]];
+then
+    echo "HEADS"
+     elif [[${Result} -eq 1 ]];
+then
+     echo "Tails"
+fi
