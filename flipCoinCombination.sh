0b514c2e14af800e0cd3513c06840b5cceabe643
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-01 09:59:58 +0530

    Added:Singlet percentage & Dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,51 @@
+
 #!/bin/bash
+RANDOM=$$
+
+PIPS=2
+MAXTHROWS=10
+throw=0
+
 Head=0
-Tils=1
-REsult=$((RANDOM%2))
-if [[ ${Result} -eq 0 ]];
-then
-    echo "HEADS"
-     elif [[${Result} -eq 1 ]];
-then
-     echo "Tails"
-fi
+Tails=0
+
+declare -A print_Result
+
+print_result ()
+{
+echo
+echo "Head =   $Head"
+echo "Tails =   $Tails"
+
+echo ("print")
+
+echo
+}
+
+update_count()
+{
+case "$1" in
+  0) let "Head += 1";;
+  1) let "Tails += 1";;
+
+esac
+}
+
+echo
+
+while [ "$throw" -lt "$MAXTHROWS" ]
+do
+  let "die1 = RANDOM % $PIPS"
+  update_count $die1
+  let "throw += 1"
+
+#define percentage
+    percent=(heads/10)*100
+
+     $percentage=(percent)
+
+
+done
+ echo "$percentage"
+print_result
+exit 0
